bc21ea627af0eca2149157e6b8837559d2369ba6
Hayden Beck <haydenabeck@gmail.com>
2021-05-06 16:59:46 -0500

    Created Models

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection')
-// import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,10 +11,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
 
-sequelize.sync().then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
